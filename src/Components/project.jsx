32ad9fd5854472f317fc23a251253adce81dcd55
abhishekredteam <110445624+abhishekredteam@users.noreply.github.com>
2022-09-28 14:54:48 +0530

    added project liks

--- src/Components/project.jsx
+++ src/Components/project.jsx
@@ -16,7 +16,8 @@ const ProjectDetails = () => {
                             {/* <h6 className="card-subtitle mb-2 text-muted">21 Sep 2022   |  Design, Patern</h6> */}
                             {/* <img src={Image10} alt="" style={{ width: '560px', height: '300px' }} /> */}
                             <p className="card-text">In this mini project we created TODO aplication using React</p>
-                   <button style={{borderRadius:"5px"}}> <Link to="/project" state={{color:"black"}}> View All</Link></button>
+                            <button style={{ borderRadius: "5px" }}> <a href="https://github.com/MrAbhishek07/Todo-app.git" target="_blank" state={{ color: "black" }}> View All</a></button>
+
 
 
                         </div>
@@ -26,7 +27,7 @@ const ProjectDetails = () => {
                             <h5 className="card-title">Wether App</h5><br />
                             {/* <h6 className="card-subtitle mb-2 text-muted">21 Sep 2022   | Figma, Icon Design</h6> */}
                             <p className="card-text">In this project we created Weather app using react which showcases the tempreture details of given places</p>
-                   <button style={{borderRadius:"5px"}}> <Link to="/project" state={{color:"black"}}> View All</Link></button>
+                            <button style={{ borderRadius: "5px" }}> <a href="https://github.com/MrAbhishek07/Weather-APP.git" target="_blank" state={{ color: "black" }}> View All</a></button>
 
 
                         </div>
@@ -39,11 +40,11 @@ const ProjectDetails = () => {
                 <div className="cards">
                     <div className="card2 bg-primary" style={{ width: "35rem" }}>
                         <div className="card-body">
-                            <h5 className="card-title">Todo-List</h5><br />
+                            <h5 className="card-title">React Form</h5><br />
                             {/* <h6 className="card-subtitle mb-2 text-muted">21 Sep 2022   |  Design, Patern</h6> */}
                             {/* <img src={Image10} alt="" style={{ width: '560px', height: '300px' }} /> */}
-                            <p className="card-text">In this mini project we created TODO aplication using React</p>
-                   <button style={{borderRadius:"5px"}}> <Link to="/project" state={{color:"black"}}> View All</Link></button>
+                            <p className="card-text">In this mini project we created form aplication using INDEXEDDB</p>
+                            <button style={{ borderRadius: "5px" }}> <a href="https://github.com/MrAbhishek07/react-form.git" target="_blank" state={{ color: "black" }}> View All</a></button>
 
 
                         </div>
@@ -53,7 +54,7 @@ const ProjectDetails = () => {
                             <h5 className="card-title">Wether App</h5><br />
                             {/* <h6 className="card-subtitle mb-2 text-muted">21 Sep 2022   | Figma, Icon Design</h6> */}
                             <p className="card-text">In this project we created Weather app using react which showcases the tempreture details of given places</p>
-                   <button style={{borderRadius:"5px"}}> <Link to="/project" state={{color:"black"}}> View All</Link></button>
+                            <button style={{ borderRadius: "5px" }}> <a href='#' state={{ color: "black" }}> View All</a></button>
 
 
                         </div>
@@ -65,7 +66,7 @@ const ProjectDetails = () => {
 
 
         </div>
-        
+
     );
 }
 
